f65ef95ced9e4405641596631efebb674238ed85
abbodev <abbodev@gmail.com>
2026-02-11 23:44:32 +0100

    chore: setup manifest and background

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,19 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
-export default defineConfig({});
+export default defineConfig({
+  manifest: ({ browser }) => ({
+    name: '5eTools Sync',
+    permissions: ['storage', 'identity'],
+    host_permissions: [
+      'https://2014.5e.tools/*',
+      'https://5e.tools/*',
+      'https://www.googleapis.com/*'
+    ],
+    background: browser === 'firefox'
+      ? { scripts: ['background.js'] }
+      : { service_worker: 'background.js' },
+    action: {
+      default_popup: 'popup/index.html'
+    }
+  })
+});
